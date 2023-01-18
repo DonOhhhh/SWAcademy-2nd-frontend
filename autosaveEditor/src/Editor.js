a44645fcddf363ddb38a36d29f0fdba1cd65558d
Dong Joon Oh <201904014@o.cnu.ac.kr>
2023-01-18 21:44:09 +0900

    자동 저장 편집기 (9)

--- autosaveEditor/src/Editor.js
+++ autosaveEditor/src/Editor.js
@@ -7,37 +7,49 @@ export default function Editor({
     const $editor = document.createElement('div')
     $target.appendChild($editor)
 
-    let isInitialized = false;
+    $editor.innerHTML = `
+    <input type="text" name="title" style="width:600px;display:block;"/>
+    <div name="content" contentEditable="true" style="width:600px; height:400px; border: 1px solid black; padding: 8px;"></div>
+    `
 
     this.state = initialState;
 
     this.setState = (nextState) => {
         this.state = nextState;
-        $editor.querySelector('[name=title]').value = this.state.title
-        $editor.querySelector('[name=content]').innerHTML = this.state.content.replace(/\n/g,'<br>')
-        // 서버에서 내려오는 개행값 \n
-        // textarea에서는 \n으로 개행을 처리해줌.
         this.render();
     }
 
     this.render = () => {
-        if (!isInitialized) {
-            $editor.innerHTML = `
-            <input type="text" name="title" style="width:600px;display:block;" value="${this.state.title}"/>
-            <div name="content" contentEditable="true" style="width:600px; height:400px;">${this.state.content}</div>
-            `
-            isInitialized = true
-        }
+        const richContent = this.state.content.split('\n').map(line => {
+            if(line.indexOf('# ') === 0) {
+                return `<h1>${line.substring(2)}</h1>`
+            } else if (line.indexOf('## ') === 0) {
+                return `<h2>${line.substring(2)}</h2>`
+            }
+            return line
+        }).join('<br>')
+        
+        $editor.querySelector('[name=title]').value = this.state.title
+        $editor.querySelector('[name=content]').innerHTML = richContent
     }
     this.render();
 
-    $editor.addEventListener('keyup', e => {
+    $editor.querySelector('[name=content]').addEventListener('input', e => {
+        const nextState = {
+            ...this.state,
+            content: e.target.innerHTML
+        }
+        this.setState(nextState);
+        onEditing(this.state);
+    })
+
+    $editor.querySelector('[name=title]').addEventListener('keyup', e => {
         const { target } = e;
         const name = target.getAttribute('name')
         if (this.state[name] !== undefined) {
             const nextState = {
                 ...this.state,
-                [name]: target.value
+                title: e.target.value
             }
             this.setState(nextState)
             onEditing(this.state)
