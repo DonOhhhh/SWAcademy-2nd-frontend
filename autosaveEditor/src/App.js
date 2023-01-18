d0c37fbc3e99128bd4904e6161ad75c258006c9d
Dong Joon Oh <201904014@o.cnu.ac.kr>
2023-01-18 11:36:52 +0900

    자동 저장 편집기 만들기 (2),(3)
    - 편집기에 내용이 업데이트 될 때마다 localStorage에 title과 content를
      묶어서 저장되는 기능
    - 연속적으로 발생하는 이벤트를 지연시키는 기능

--- autosaveEditor/src/App.js
+++ autosaveEditor/src/App.js
@@ -1,8 +1,29 @@
+import Editor from "./Editor.js";
 import PostsPage from "./PostsPages.js";
+import { getItem, setItem } from "./Storage.js";
 
 export default function App({ $target }) {
-    const postPage = new PostsPage({
-        $target
+    // const postPage = new PostsPage({
+    //     $target
+    // })
+    // postPage.render();
+    const TEMP_POST_SAVE_KEY = 'temp-post'
+    const post = getItem(TEMP_POST_SAVE_KEY, {
+        title: '',
+        content: ''
     })
-    postPage.render();
+    let timer = null;
+    const editor = new Editor({
+        $target,
+        initialState: post,
+        onEditing: (post) => {
+            // 이벤트가 연속으로 발생하고 있을 때는 이벤트를 계속 지연시키다가 마지막으로 이벤트가 발생하고 일정 시간이 지났을 때 원래 지연시켰던 이벤트를 실행시킨다.
+            timer = setTimeout(() => {
+                setItem(TEMP_POST_SAVE_KEY, {
+                    ...post,
+                    tempSaveData: new Date()
+                })
+            }, 2000)
+        }
+    })
 }
