666620bd4306066e9aceb66b7ca677f253559c8b
Dong Joon Oh <201904014@o.cnu.ac.kr>
2023-01-26 03:24:08 +0900

    컴포넌트 스타일링

--- React/01/kdt-app/src/App.js
+++ React/01/kdt-app/src/App.js
@@ -1,23 +1,12 @@
-import { useState } from "react";
-import Board from "./components/Board";
-import Pagination from "./components/Pagination/Pagination";
+import Box from "./components/Box";
 
 function App() {
-  const [page, setPage] = useState(0);
-  const articles = new Array(100).fill().map((_, i) => ({
-    id: i,
-    title: `${i}번 게시물`
-  }))
-
-  const limit = 10;
-  const offset = page * limit;
-
   return (
     <div>
-      <Pagination defaultPage={0} limit={10} total={articles.length} onChange={setPage} />
-      <Board articles={articles.slice(offset, offset + limit)} />
+      <div css={{ width: 200, height: 100, backgroundColor: "black" }} />
+      <Box />
     </div>
-  );
+  )
 }
 
 export default App;
