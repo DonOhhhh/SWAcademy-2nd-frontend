4fe5f84e46011ba3c98b449830aae48bcd6db89e
Dong Joon Oh <201904014@o.cnu.ac.kr>
2023-01-26 12:55:59 +0900

    Revert "상태와 이벤트 바인딩"
    This reverts commit 4dcf75779122e805330c9b327912bb612952f67d.

--- React/01/kdt-app/src/App.js
+++ React/01/kdt-app/src/App.js
@@ -1,24 +1,34 @@
 import { useState } from 'react';
 import './App.css';
-import Counter from './components/Counter';
+import Board from './components/Board';
 
 function App() {
-  const [totalCount, setTotalCount] = useState(0);
-  return (<div>
-    TotalCount: {totalCount}
-    <Counter
-      onIncrease={count => setTotalCount(totalCount + 1)}
-      onDecrease={count => setTotalCount(totalCount - 1)}
-    />
-    <Counter
-      onIncrease={count => setTotalCount(totalCount + 1)}
-      onDecrease={count => setTotalCount(totalCount - 1)}
-    />
-    <Counter
-      onIncrease={count => setTotalCount(totalCount + 1)}
-      onDecrease={count => setTotalCount(totalCount - 1)}
-    />
-  </div>);
+
+  const [visible, setVisible] = useState(false);
+
+  const articles = [{
+    id: 1,
+    title: '안녕하세요 프롱이 여러분',
+    author: '이선협'
+  }, {
+    id: 2,
+    title: '프로젝트를 성공시키는 법',
+    author: '이선협'
+  }, {
+    id: 3,
+    title: 'React 꿀팁 공유',
+    author: '김선협'
+  }, {
+    id: 4,
+    title: '7일만에 React 마스터하기',
+    author: '박선협'
+  }]
+
+  return <div>
+    <button onClick={() => setVisible(!visible)}>Toggle</button>
+    {visible && <h1>논리곱 연산자를 통해 쉽게 JSX 렌더링 여부를 결정할 수 있다.</h1>}
+    {visible ? <Board articles={articles} /> : <p>게시판을 보려면 Toggle 버튼을 클릭해주세요.</p>}
+  </div>;
 }
 
 export default App;
