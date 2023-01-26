2b84c8df27b701b4e567dd6267cf17f3d0eb72cd
Dong Joon Oh <201904014@o.cnu.ac.kr>
2023-01-26 12:55:58 +0900

    Revert "useEffect"
    This reverts commit f2f03e2a10121b426806bd543a8d907964e34b9a.

--- React/01/kdt-app/src/App.js
+++ React/01/kdt-app/src/App.js
@@ -1,26 +1,24 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react';
+import './App.css';
+import Counter from './components/Counter';
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  useEffect(() => {
-    console.log(`Clicked ${count} times.`)
-  }, [count]) // count의 변화를 감지한다.
-
-  useEffect(() => {
-    console.log('Component Loaded')
-    const handleScroll = () => {
-      console.log(window.scrollY);
-    }
-    document.addEventListener('scroll',handleScroll) // 전역적인 이벤트를 사용할 때 쓸 수 있다.
-    return () => document.removeEventListener('scroll', handleScroll); // return으로 반환한 함수는 컴포넌트가 제거될 때 실행된다.
-  }, []); // 컴포넌트가 처음 로드될 때 실행된다.
-
-  return <div>
-    <div>You Clicked {count} times.</div>
-    <button onClick={() => setCount(count + 1)}>+</button>
-    <div style={{height: 10000}}></div>
-  </div>;
+  const [totalCount, setTotalCount] = useState(0);
+  return (<div>
+    TotalCount: {totalCount}
+    <Counter
+      onIncrease={count => setTotalCount(totalCount + 1)}
+      onDecrease={count => setTotalCount(totalCount - 1)}
+    />
+    <Counter
+      onIncrease={count => setTotalCount(totalCount + 1)}
+      onDecrease={count => setTotalCount(totalCount - 1)}
+    />
+    <Counter
+      onIncrease={count => setTotalCount(totalCount + 1)}
+      onDecrease={count => setTotalCount(totalCount - 1)}
+    />
+  </div>);
 }
 
 export default App;
