f07ec0b829e17cc03ff6ad8ffa28997bf3b1ea3e
Dong Joon Oh <201904014@o.cnu.ac.kr>
2023-01-26 12:23:59 +0900

    Custom Hook

--- React/01/my-app/src/App.js
+++ React/01/my-app/src/App.js
@@ -1,6 +1,23 @@
+import Box from "./components/Box";
+import Checkbox from "./components/Checkbox";
+import useHover from "./hooks/useHover";
+import useKeyPress from "./hooks/useKeyPress";
+import useToggle from "./hooks/useToggle";
+
 function App() {
+  const [on, toggle] = useToggle();
+  const [ref, isHover] = useHover();
+  const keyPressed = useKeyPress('a');
+
   return (
-    <div>1</div>
+    <div>
+      <Checkbox checked={on} onChange={toggle} />
+      {/* <button onClick={toggle}>{on ? 'True' : 'False'}</button> */}
+      {isHover ? 'hover' : 'mouseout'}
+      <Box ref={ref} />
+
+      {keyPressed && 'Pressed'}
+    </div>
   )
 }
 
