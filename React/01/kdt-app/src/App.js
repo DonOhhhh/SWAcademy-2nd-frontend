a16c8bc5ae8ed9427822ae0880496c6f68ed9647
Dong Joon Oh <201904014@o.cnu.ac.kr>
2023-01-26 09:11:49 +0900

    useCallback

--- React/01/kdt-app/src/App.js
+++ React/01/kdt-app/src/App.js
@@ -1,14 +1,20 @@
-import { useState } from "react";
-import Box from "./components/React.memo/Box";
+import { useCallback, useState } from "react";
+import Checkbox from "./components/useCallback/Checkbox";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [foodOn, setFoodOn] = useState(false);
+  const [clothesOn, setClothesOn] = useState(false);
+  const [shelterOn, setShetlerOn] = useState(false);
+
+  const foodChange = useCallback(e => setFoodOn(e.target.checked), []);
+  const clothesChange = useCallback(e => setClothesOn(e.target.checked), []);
+  const shelterChange = useCallback(e => setShetlerOn(e.target.checked), []);
 
   return (
     <div>
-      {count}
-      <button onClick={() => setCount(count + 1)}>+</button>
-      <Box />
+      <Checkbox label="Food" on={foodOn} onChange={foodChange} />
+      <Checkbox label="Clothes" on={clothesOn} onChange={clothesChange} />
+      <Checkbox label="Shelter" on={shelterOn} onChange={shelterChange} />
     </div>
   )
 }
